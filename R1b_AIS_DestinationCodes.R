f3040e854a7d749febee74f54aee7024a2c7828c
kellykapsar <kelly.kapsar@gmail.com>
2023-04-27 13:35:09 -0500

    Create file to incorporate manual revisions to destination codes into vessel track lines.

---
+++ R1b_AIS_DestinationCodes.R
@@ -0,0 +1,26 @@
+################################################################################
+# TITLE: Destination coding addition to vector data
+# PURPOSE: This script takes monthly vector files by ship type and adds manually
+# interpreted revisions of destination codes to individual track lines. 
+# AUTHOR: Kelly Kapsar & Bella Block
+# CREATED: 2023-04-27
+# LAST UPDATED ON: 2023-04-27
+# 
+# NOTE: 
+################################################################################
+
+# Load libraries
+library(sf)
+library(dplyr)
+
+
+
+# Create list of files 
+vpath <- "D:/AIS_V2_DayNight_60km6hrgap/Vector/"
+
+vecs <- list.files(vpath, pattern='.shp', full.names = F)
+vecsfull <- list.files(vpath, pattern='.shp', full.names = T)
+
+# Read in codes. 
+
+
